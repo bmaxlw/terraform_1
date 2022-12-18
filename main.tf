9c06b5960dea9249840b6e5e2d5d72d16ebe7ebb
Maksym Bondaruk <b.max.lw@gmail.com>
2022-12-18 13:37:28 +0200

    Initial config + EC2 to test access

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_instance" "app_server" {
+  ami           = var.ec2_instance_ami
+  instance_type = var.ec2_instance_type
+
+  tags = {
+    Name = "dev_1"
+  }
+}
